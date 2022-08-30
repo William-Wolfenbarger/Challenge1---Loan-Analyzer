a1e10d749a70d0c5b8ff72e4fb760eeacad49f33
William-Wolfenbarger <111939161+William-Wolfenbarger@users.noreply.github.com>
2022-08-30 00:47:17 -0400

    Add files via upload
    Uploading challenge 1 files for challenge 1

---
+++ Challenge1_Files.py/Challenge1.py
@@ -0,0 +1,15 @@
+
+
+loan_costs = [500, 600, 200, 1000, 450]
+
+number_of_loans = len(loan_costs)
+print("There total number of outstanding loans is: ", number_of_loans)
+
+total_loan_cost = sum(loan_costs)
+print("The total value of all loans in $ is: ", total_loan_cost)
+
+average_loan_cost = total_loan_cost / number_of_loans
+print("The average loan cost is: ", average_loan_cost)
+
+
+
